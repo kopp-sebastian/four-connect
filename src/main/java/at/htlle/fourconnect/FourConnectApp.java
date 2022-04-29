b98f016126d8951c8a7a66c61ce90a2940772786
LucaVeit <191witb26@o365.htl-leoben.at>
2022-04-29 11:07:47 +0200

    Sprint 2 unfinished

--- src/main/java/at/htlle/fourconnect/FourConnectApp.java
+++ src/main/java/at/htlle/fourconnect/FourConnectApp.java
@@ -2,6 +2,8 @@ package at.htlle.fourconnect;
 
 import java.util.Scanner;
 
+import static at.htlle.fourconnect.View.printPattern;
+
 public class FourConnectApp {
     //We need to first create the basic visual pattern
     public static String[][] createPattern()
@@ -37,19 +39,6 @@ public class FourConnectApp {
         return f;
     }
 
-    //Yes, we even need to make a new method for visually
-    //printing our game, but at least it's not hard to do
-    public static void printPattern(String[][] f)
-    {
-        for (int i =0;i<f.length;i++)
-        {
-            for (int j=0;j<f[i].length;j++)
-            {
-                System.out.print(f[i][j]);
-            }
-            System.out.println();
-        }
-    }
 
     //Here's are basic move, making the lowest empty row
     //of a specific column have a Red
--- src/main/java/at/htlle/fourconnect/View.java
+++ src/main/java/at/htlle/fourconnect/View.java
@@ -4,4 +4,23 @@ public class View {
     public void printView(String text){
         System.out.println(text);
     }
+
+    public void printlnView(String text){
+        System.out.println(text);
+    }
+
+    //Yes, we even need to make a new method for visually
+    //printing our game, but at least it's not hard to do
+    public static void printPattern(String[][] f)
+    {
+        for (int i =0;i<f.length;i++)
+        {
+            for (int j=0;j<f[i].length;j++)
+            {
+                System.out.print(f[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
 }
